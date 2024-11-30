c5066bc9b2059c7eee50e81421bc772b4bc6ba1c
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-11-30 21:18:12 +0200

    Added CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "./utils/math.cpp"
 
 int main(int argc, char *argv[]) {
-  print_first_n_amount_of_mersenne_prime_nums<unsigned long long>(9);
+  print_first_n_amount_of_mersenne_prime_nums<unsigned long long>(5);
   return 0;
 }
