1caed9f1a29d694180a95c9e5c5d81c0bd947dc1
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-12-08 01:31:55 +0200

    Add divisors testing

---
+++ test/cases/test_divisors.cpp
@@ -0,0 +1,34 @@
+#include "./test_divisors.h"
+#include "../../src/utils/divisors.cpp"
+#include "../../src/utils/primes.cpp"
+#include <climits>
+#include <gtest/gtest.h>
+
+TEST(DivisorsTest, Assertions) {
+  const int of = 180;
+  std::vector<int> expected_divisors = {2,  3,  4,  5,  6,  9,  10, 12,
+                                        15, 18, 20, 30, 36, 45, 60, 90};
+  std::vector<int> received_divisors = get_divisors(of);
+  ASSERT_EQ(expected_divisors, received_divisors);
+
+  return;
+}
+
+TEST(DifferenceTest, OneEdgeCases) {
+  for (int i = 2; i < 500; i++) {
+    if (is_prime(i)) {
+      ASSERT_EQ(std::vector<int>{i}, get_divisors(i * i));
+    }
+  }
+
+  return;
+}
+
+TEST(DifferenceTest, ZeroEdgeCase) {
+  const short of = SHRT_MIN;
+  std::vector<short> expected_divisors = {};
+  std::vector<short> received_divisors = get_divisors(of);
+  ASSERT_EQ(expected_divisors, received_divisors);
+
+  return;
+}
