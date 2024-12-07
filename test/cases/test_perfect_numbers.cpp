08fbe9b44df7083b9ede59f111469962c2d90384
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-12-07 21:16:32 +0200

    Added a test case for the new perfect_numbers with iota range fn

--- test/cases/test_perfect_numbers.cpp
+++ test/cases/test_perfect_numbers.cpp
@@ -17,3 +17,18 @@ TEST(PerfectNumbersTest, Assertions) {
 
   return;
 }
+
+TEST(PerfectNumbersTestRanges, Assertions) {
+  std::ranges::iota_view<int, int> range = std::ranges::views::iota(6, 8129);
+  std::vector<int> perfect_nums = get_perfect_nums_from_range(range);
+  std::vector<int> expected_perfect_nums = {6, 28, 496, 8128};
+
+  ASSERT_EQ(perfect_nums, expected_perfect_nums);
+
+  // Check if all numbers are perfect
+  for (const int &num : perfect_nums) {
+    EXPECT_TRUE(is_perfect(num));
+  }
+
+  return;
+}
