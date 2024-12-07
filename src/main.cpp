a58224c61bcae37cba202ae0fb7f35c21ada5c5a
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-12-07 23:47:54 +0200

    Added more printing in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,8 @@
 
 int main(int argc, char *argv[]) {
   print_first_n_amount_of_mersenne_prime_nums<unsigned long long>(5);
+  print_divisors(141235);
+  print_mersenne_nums_in_interval(0, 10999);
+  print_prime_factors(6469693230);
   return 0;
 }
