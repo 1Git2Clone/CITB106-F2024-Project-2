cc966e9fc97d5a671028896f51660af2d809a3f0
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-12-08 16:07:02 +0200

    Add user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,50 @@
 #include "./utils/divisors.hpp"
 #include "./utils/mersenne.hpp"
+#include "utils/helper_functions.hpp"
 #include "utils/primes.hpp"
+#include <format>
+#include <limits>
+
+int user_input_numbers() {
+  int n_amount_of_mersenne_primes = get_integer_input_line(
+      std::format("Enter number between {} and {} (In order to get the fisrt "
+                  "<input> amount of mersenne primes): ",
+                  1, 9),
+      1, 9);
+
+  std::cout << "First " << n_amount_of_mersenne_primes << " mersenne primes: ";
+  print_first_n_amount_of_mersenne_prime_nums<unsigned long long>(
+      n_amount_of_mersenne_primes);
+  std::cout << std::flush;
+
+  int to_get_divisors_from = get_integer_input_line(
+      std::format("Enter number between {} and {} to get its divisors from: ",
+                  0, std::numeric_limits<int>::max()),
+      0, std::numeric_limits<int>::max());
+  std::cout << "Divisors of " << to_get_divisors_from << ": ";
+  print_divisors(to_get_divisors_from);
+  std::cout << std::flush;
+
+  int mersenne_number_interval_begin =
+      get_integer_input_line<int>("Enter mersenne number interval begin: ");
+  int mersenne_number_interval_end = get_integer_input_line(
+      "Enter mersenne number interval end: ", mersenne_number_interval_begin);
+  std::cout << "Mersenne numbers between " << mersenne_number_interval_begin
+            << " and " << mersenne_number_interval_end << ": ";
+  print_mersenne_nums_in_interval(mersenne_number_interval_begin,
+                                  mersenne_number_interval_end);
+  std::cout << std::flush;
+
+  long prime_factors_n = get_integer_input_line<long>(
+      "Enter a long int to get its prime factors from: ");
+  std::cout << "Prime factors of " << prime_factors_n << ": ";
+  print_prime_factors(prime_factors_n);
+  std::cout << std::flush;
+
+  return 0;
+}
 
 int main(int argc, char *argv[]) {
-  print_first_n_amount_of_mersenne_prime_nums<unsigned long long>(5);
-  print_divisors(141235);
-  print_mersenne_nums_in_interval(0, 10999);
-  print_prime_factors(6469693230);
+  user_input_numbers();
   return 0;
 }
--- src/utils/helper_functions.hpp
+++ src/utils/helper_functions.hpp
@@ -4,7 +4,9 @@
 #include "concepts.hpp"
 
 #include <iostream>
+#include <limits>
 #include <set>
+#include <type_traits>
 #include <vector>
 
 template <Integer T> bool binary_search(T val, const std::vector<T> &from) {
@@ -51,6 +53,45 @@ template <Integer T> void print_set(const std::set<T> &v, const char *msg) {
   for (const T &i : v)
     std::cout << i << " ";
   std::cout << "}" << std::endl;
+}
+
+template <Integer T>
+// Weird behaviour for unsigned ints with negative input (doesn't trigger
+// `std::cin.fail()`).
+  requires std::is_signed_v<T>
+T get_integer_input_line(const std::string &msg,
+                         const T &min = std::numeric_limits<T>::min(),
+                         const T &max = std::numeric_limits<T>::max()) {
+  T input;
+  while (true) {
+    std::cout << msg;
+    std::cin >> input;
+
+    if (std::cin.eof())
+      exit(1);
+
+    if (std::cin.fail()) {
+      std::cout << "Invalid input." << std::endl;
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      continue;
+    }
+
+    if (input < min) {
+      std::cout << "Input was less than " << min << ". Defaulting to " << min
+                << std::endl;
+      input = min;
+    }
+    if (input > max) {
+      std::cout << "Input was greater than " << max << ". Defaulting to " << max
+                << std::endl;
+      input = max;
+    }
+
+    break;
+  };
+
+  return input;
 }
 
 #endif // !HELPER_FUNCTIONS_HPP
